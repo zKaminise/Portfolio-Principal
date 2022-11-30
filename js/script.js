8cc56887cd7f40ea0ab15377936b20f6cbd3d108
Gabriel Misao Pinheiro Kaminise <117327879+zKaminise@users.noreply.github.com>
2022-11-30 02:00:14 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*================================ Typing animation =================== */
+var typed = new Typed(".typing",{
+    strings:["","Programador Junior Full-Stack"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
